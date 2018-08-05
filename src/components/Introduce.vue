12d513d3f157d3966a8fb04b646c30e0fb7d59c4
yilz <yi_leizhao@163.com>
2018-08-05 16:48:47 +0800

    增加信息展示

---
+++ src/components/Introduce.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="backg">
+    <img src="../assets/border.png">
+    <p>{{sexSelect}}</p>
+    <div>
+      <img src="../assets/male01.png" class="male">
+      <img src="../assets/male02.png" class="male">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Introduce',
+  data () {
+    return {
+      sexSelect: '性格选择'
+    }
+  }
+}
+</script>
+<style lang="scss">
+  .backg img{
+    width: 89%;
+    padding-top: 1.6rem;
+  }
+  p{
+    margin-top: -29rem;
+    font-size: 16px;
+    color: #44403f;
+    font-weight: bold;
+  }
+  .male{
+    width: 18%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Question from '@/components/Question'
+import Introduce from '@/components/Introduce'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
       path: '/question',
       name: 'question',
       component: Question
+    }, {
+      path: '/introduce',
+      name: 'introduce',
+      component: Introduce
     }
   ]
 })
