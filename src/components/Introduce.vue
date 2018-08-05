6064ca0ccdff5f0d79d2be83ca1ae10c9395e10d
yilz <yi_leizhao@163.com>
2018-08-05 22:25:24 +0800

    性别选择

--- src/components/Introduce.vue
+++ src/components/Introduce.vue
@@ -1,10 +1,21 @@
 <template>
-  <div class="backg">
-    <img src="../assets/border.png">
+  <div >
+    <img class="backg" src="../assets/background_03.png">
     <p>{{sexSelect}}</p>
-    <div>
-      <img src="../assets/male01.png" class="male">
-      <img src="../assets/male02.png" class="male">
+    <div class="gender-image">
+      <div class="blank"></div>
+      <div class="blank"></div>
+      <div >
+        <img v-if="isMale" src="../assets/male.png" class="gender" @click='changeGender("male")'>
+        <img v-else src="../assets/male_unchecked.png" class="gender" @click='changeGender("male")'>
+      </div>
+      <div class="blank"></div>
+      <div>
+        <img v-if="isFemale" src="../assets/female.png" class="gender" @click='changeGender("female")'>
+        <img v-else src="../assets/female_unchecked.png" class="gender" @click='changeGender("female")'>
+      </div>
+      <div class="blank"></div>
+      <div class="blank"></div>
     </div>
   </div>
 </template>
@@ -13,23 +24,44 @@ export default {
   name: 'Introduce',
   data () {
     return {
-      sexSelect: '性格选择'
+      sexSelect: '性格选择',
+      isMale: false,
+      isFemale: false
+    }
+  },
+  methods: {
+    changeGender (gender) {
+      if (gender === 'male') {
+        this.isFemale = this.isMale
+        this.isMale = !this.isMale
+      } else {
+        this.isMale = this.isFemale
+        this.isFemale = !this.isFemale
+      }
     }
   }
 }
 </script>
 <style lang="scss">
-  .backg img{
+  .backg {
     width: 89%;
     padding-top: 1.6rem;
   }
   p{
-    margin-top: -29rem;
+    margin-top: -121%;
     font-size: 16px;
     color: #44403f;
     font-weight: bold;
   }
-  .male{
-    width: 18%;
+  .gender-image{
+    display: flex;
+    margin-top: 4%;
+  }
+  .blank{
+    flex-grow: 1;
+  }
+  .gender{
+    flex-grow: 3;
+    width: 6rem;
   }
 </style>
